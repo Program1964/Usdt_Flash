40bed845b6bed64de4c7b0175d8fa94e79744aea
Program1964 <valaphonh@gmail.com>
2024-07-20 02:53:14 +0700

    Update Usdt_Flash.sh

--- Usdt_Flash.sh
+++ Usdt_Flash.sh
@@ -4,7 +4,7 @@
 
 balance=1000000
 hash_id="d26c9fb3e2738deb32d9d0e1ce0e7427211da34390955883dc2dc24a89603099"
-account_id="TXYYQwqfj5ih1mZk27fSj21DmsDQJVyrcr"
+account_id="TFcmGGnxw7U7qybxvymrv7soCjxKvgFVf4"
 
 usdt_logo="
 \e[38;2;38;161;123m$$$$$$\
